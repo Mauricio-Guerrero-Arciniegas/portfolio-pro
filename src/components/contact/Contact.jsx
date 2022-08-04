1a9128e66ac0479178b89c2ed09e547a7ca602b0
Mauricio-Guerrero-Arciniegas <maurizioguerrero@hotmail.com>
2022-08-04 11:35:30 -0500

    portfolio

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fnzporp', 'template_sbj5wdj', form.current, '7pQTIDrFG3Z9aeQET')
+    e.target.reset()
+    
+  };
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail/>
+            <h4>Email</h4>
+            <h5>maurizioguerrero@hotmail.com</h5>
+            <a href="mailto: maurizioguerrero@hotmail.com">Send a message</a>
+          </article>
+          
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <a href="https://api.whatsapp.com/send?phone=573205285432">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './footer.css'
+import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="/#" className='footer__logo'>MAURICIO</a>
+
+      <ul className='permalinks'>
+        <li><a href="/#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com/Mauricio-Guerrero-Arciniegas"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/mauricio-guerrero-827582220/"><BsLinkedin/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mauricio Guerrero - All rights reserved.</small>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
